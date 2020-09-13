9dcbb445b9ca9aa990a354820f0f2b66f182607f
Gary Barzola <gnbarzol@espol.edu.ec>
2020-09-13 00:34:35 -0500

    Updates

---
+++ precache-manifest.81b79a84f84513d4c6bf30d929f07fcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2409f458e35809f3912d207a75fb03f3",
+    "url": "/certificados/index.html"
+  },
+  {
+    "revision": "9e845fefee384fd15d50",
+    "url": "/certificados/static/css/2.31a97525.chunk.css"
+  },
+  {
+    "revision": "5ffb49ec31fbccd9ed91",
+    "url": "/certificados/static/css/main.359a6688.chunk.css"
+  },
+  {
+    "revision": "9e845fefee384fd15d50",
+    "url": "/certificados/static/js/2.3ebd8502.chunk.js"
+  },
+  {
+    "revision": "4fe970998a22871c0107e95099a4dbbc",
+    "url": "/certificados/static/js/2.3ebd8502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ffb49ec31fbccd9ed91",
+    "url": "/certificados/static/js/main.293fe725.chunk.js"
+  },
+  {
+    "revision": "f23c202387b5c7cfe09b",
+    "url": "/certificados/static/js/runtime-main.0dbaabae.js"
+  }
+]);
